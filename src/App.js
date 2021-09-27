bd556441d04162dcf4f6125fea86ff43472e100c
Mirukai <arthassvitaliy@gmail.com>
2021-09-27 19:03:58 +0300

    add slideStop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import { Component } from 'react';
-import Counter from "./components/Counter"
+import Slider from './components/Slider ';
+import "./sliderButton.css"
+
+const data = [
+  { link: "https://cdn.profile.ru/wp-content/uploads/2021/04/chashka-kofe-zerna-lopatka.jpg" },
+  { link: "https://cdn.profile.ru/wp-content/uploads/2021/08/Kofe-zerna-korica.jpg" },
+  { link: "https://cdn.profile.ru/wp-content/uploads/2021/06/kofe-zerna-kofemolka-chashka.jpg" },
+]
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      step:1
+      index: 0,
+      interval: 1500
     }
   }
-  changeStep = (e) => {
-    const value = parseInt(e.target.value)
+  slide = () => {
+    return this.setState((state) => {
+      return { index: state.index !== 2 ? state.index + 1 : state.index = 0 }
+    })
+  }
+  slideShow = () => {
+    return this.setState((state) => {
+      return setInterval((this.slide), state.interval)
+    })
+  }
+  slideStop = () => {
+    return clearInterval(this.slideShow)
+  }
+  slideSpeed = (e) => {
     return this.setState(() => {
-      return {step: value}
-    }) 
+      return {interval: e.target.value}
+    })
   }
   render() {
-    const {step} = this.state
-    return <> 
-    <Counter step={step} /> 
-    <input value={step} type="number" onChange={this.changeStep}></input>
+    const { index, interval } = this.state
+    return <>
+      <div className="slider">
+        <Slider data={data[index]} />
+        <button className="sliderButton" onClick={this.slide} />
+        <button className="sliderButton" onClick={this.slideShow} />
+        <input type="number" value={(interval)} onChange={this.slideSpeed}/>
+        <button onChange={this.slideStop}>stop</button>
+      </div>
     </>
   }
 }
